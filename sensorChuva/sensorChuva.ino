e2a883a87ffb7a180b2bf676e6ae7e1056c86aa8
Odirlei Vial Machado <odirleivial@gmail.com>
2020-09-08 13:47:45 -0300

    Adição do envio do sinal de rádio
    Adição do envio do sinal de rádio

--- sensorChuva/sensorChuva.ino
+++ sensorChuva/sensorChuva.ino
@@ -6,45 +6,50 @@ Data Criação: 04/09/2020
 Descrição: Projeto com um sensor de chuva e um transmissor de rádio frequência para fechar janela em caso de chuva. 
 */
 
+#define pin_bt 7 // Botão
+#define pin_led 13 // Led
 #define pinChuva 3 //Pino do sensor de chuva 
-#include <RCSwitch.h>
-RCSwitch mySwitch = RCSwitch(); // ligar o transmissor de RF no pino 2
 
+#include <RCSwitch.h>
+RCSwitch RF433Send = RCSwitch(); // ligar o transmissor de RF no pino 10
 
 int count_chuva = 0;
-long tempo_verificacao =  60000; //tempo em ms para fazer a verificação se está chevendo e mandar o comando de fechar a janela
+long tempo_verificacao =  10000; //tempo em ms para fazer a verificação se está chevendo e mandar o comando de fechar a janela
+long tempo_normal = 1000; //60000
+long tempo_chuva = 1000; //300000
 long tempo_inicial = millis();
-
-// Dados do controle remoto:
-//  botão de fechar: Received 154058277 / 28bit Protocol: 6
-//  botão de abrir:  Received 154058261 / 28bit Protocol: 6
-//  botão de pausa:  Received 154058293 / 28bit Protocol: 6
-long codigo_controle = 154058277;
-int  tamanho_pacote = 28; 
-int protocolo_controle = 6;
-
+long blink_inicial = millis();
 
 void setup() {
     pinMode(pinChuva, INPUT);
+    pinMode(pin_bt, INPUT_PULLUP);    
+    pinMode(pin_led, OUTPUT);        
+    digitalWrite(pin_led,LOW);
+    
     Serial.begin(9600);
 
-    mySwitch.enableTransmit(10);
-    mySwitch.setProtocol(protocolo_controle);
-    mySwitch.setRepeatTransmit(5);
+    RF433Send.enableTransmit(10);
+    RF433Send.setProtocol(4);
 }
 
 void loop() {
-    //verifica se atigiu o tempo definido na variável tempo_verificacao, 
-    // se o tempo foi atingido envia o sinal para fechar a janela através da função fecha_janela()
-    if (tempo_inicial + tempo_verificacao >= millis()){
-        tempo_inicial = millis();
-        if (status_chuva()){
-            fecha_janela();
-            tempo_verificacao = 300000; //altera o tempo de vefificação para 5 minutos caso esteja chevendo
-        }else{
-            tempo_verificacao = 60000; //mantem o tempo de verificação em 1 minuto caso não esteja chevendo
-        }
-    } 
+  //verifica se atigiu o tempo definido na variável tempo_verificacao, 
+  // se o tempo foi atingido envia o sinal para fechar a janela através da função fecha_janela()
+  if(tempo_inicial + tempo_verificacao <= millis()){
+    if(status_chuva()){
+      fn_blink(5);
+      fecha_janela();
+      tempo_inicial = millis();
+      tempo_verificacao = tempo_chuva; //altera o tempo de vefificação para 5 minutos caso esteja chevendo
+    }else{
+      tempo_verificacao = tempo_normal; //mantem o tempo de verificação em 1 minuto caso não esteja chevendo
+    }
+  } 
+  
+  if(!digitalRead(pin_bt)){
+    fecha_janela();
+    fn_blink(5);
+  }
 }
 
 bool status_chuva(){
@@ -68,6 +73,25 @@ bool status_chuva(){
 
 void fecha_janela(){
     // Envia código do botão de fechar a janela
-    mySwitch.send(codigo_controle, tamanho_pacote);
+    RF433Send.sendQuadState("11F0001Q1Q10FF0F0101");
+          Serial.println("FECHA!");
 
+
+}
+
+void fn_blink(int qtd){
+  int aux = 0;
+  while (aux < qtd){
+    if (blink_inicial + 150 <= millis()){
+      blink_inicial = millis();
+      
+      if(!digitalRead(pin_led)){
+        digitalWrite (pin_led,HIGH);
+      }else{
+        digitalWrite (pin_led,LOW);
+        aux++;
+      }
+    }
+  }
+  digitalWrite (pin_led,LOW);
 }
